3a2c4ac63b9a2497daf33ff8a9d6b6777da8974b
Manny <mnvergel@gmail.com>
2014-11-27 13:52:10 +0800

    new blog

--- routes.js
+++ routes.js
@@ -15,7 +15,7 @@ module.exports = function(pluginConf, web) {
 	}
   }
 
-  routes['/css/plugin/blog/ghostdown.css'] = {
+  /*routes['/css/plugin/blog/ghostdown.css'] = {
     get: function(req, res) {
       web.utils.serveStaticFile(pluginConf.pluginPath + '/pub/ghostdown.css', res);
     }
@@ -30,6 +30,66 @@ module.exports = function(pluginConf, web) {
   routes['/js/plugin/blog/jquery.ghostdown.js'] = {
     get: function(req, res) {
       web.utils.serveStaticFile(pluginConf.pluginPath + '/pub/jquery.ghostdown.js', res);
+    }
+  }*/
+
+  routes['/css/plugin/blog/bootstrap.min.css'] = {
+    get: function(req, res) {
+      web.utils.serveStaticFile(pluginConf.pluginPath + '/pub/bootstrap.min.css', res);
+    }
+  }
+
+  routes['/css/plugin/blog/bootstrap-markdown.min.css'] = {
+    get: function(req, res) {
+      web.utils.serveStaticFile(pluginConf.pluginPath + '/pub/bootstrap-markdown.min.css', res);
+    }
+  }
+
+  routes['/css/plugin/blog/fonts/glyphicons-halflings-regular.eot'] = {
+    get: function(req, res) {
+      web.utils.serveStaticFile(pluginConf.pluginPath + '/pub/fonts/glyphicons-halflings-regular.eot', res);
+    }
+  }
+
+  routes['/css/plugin/blog/fonts/glyphicons-halflings-regular.svg'] = {
+    get: function(req, res) {
+      web.utils.serveStaticFile(pluginConf.pluginPath + '/pub/fonts/glyphicons-halflings-regular.svg', res);
+    }
+  }
+
+  routes['/css/plugin/blog/fonts/glyphicons-halflings-regular.ttf'] = {
+    get: function(req, res) {
+      web.utils.serveStaticFile(pluginConf.pluginPath + '/pub/fonts/glyphicons-halflings-regular.ttf', res);
+    }
+  }
+
+  routes['/css/plugin/blog/fonts/glyphicons-halflings-regular.woff'] = {
+    get: function(req, res) {
+      web.utils.serveStaticFile(pluginConf.pluginPath + '/pub/fonts/glyphicons-halflings-regular.woff', res);
+    }
+  }
+
+  routes['/js/plugin/blog/bootstrap.min.js'] = {
+    get: function(req, res) {
+      web.utils.serveStaticFile(pluginConf.pluginPath + '/pub/bootstrap.min.js', res);
+    }
+  }
+
+  routes['/js/plugin/blog/bootstrap-markdown.js'] = {
+    get: function(req, res) {
+      web.utils.serveStaticFile(pluginConf.pluginPath + '/pub/bootstrap-markdown.js', res);
+    }
+  }
+
+  routes['/js/plugin/blog/markdown.js'] = {
+    get: function(req, res) {
+      web.utils.serveStaticFile(pluginConf.pluginPath + '/pub/markdown.js', res);
+    }
+  }
+
+  routes['/js/plugin/blog/to-markdown.js'] = {
+    get: function(req, res) {
+      web.utils.serveStaticFile(pluginConf.pluginPath + '/pub/to-markdown.js', res);
     }
   }
 
